287f31fff16357821b83cbadbe98df47c238d660
12999367 <12999367@SISTEMI03>
2025-11-18 08:40:44 +0100

    Aggiungere i file di progetto.

---
+++ GiocoDellaVita/CPersonaggio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiocoDellaVita
+{
+    abstract class CPersonaggio
+    {
+        private int _salute;
+        private int _stamina;
+        private int[,] _posizione;
+
+        public int Salute
+        {
+            get { return _salute; }
+            set { _salute = value; }
+        }
+
+        public int Stamina
+        {
+            get { return _stamina; }
+            set { _stamina = value; }
+        }
+
+        public CPersonaggio() { }
+
+
+    }
+}
